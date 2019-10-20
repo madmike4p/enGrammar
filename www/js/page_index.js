b5c3312af30b46912f304a1e8a46db84151e3875
madmike4p <madmike.4p@gmail.com>
2019-10-20 06:44:15 +0200

    changes, ok

--- www/js/page_index.js
+++ www/js/page_index.js
@@ -9,15 +9,66 @@ var app = {
     }, // end bindEvents
     
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        
-        alert(books[2][3][10].pl + '\n' + books[2][3][10].pl);
+      app.receivedEvent('deviceready');
+
+      $(".navBook").on('click', function () {
+        app.currentBook.book = parseInt(this.getAttribute("data-id"));
+        $.mobile.changePage('#chapterPage');
+        return false;
+      });
+
+      $(document).on('pagebeforeshow','#chapterPage',  app.prepareBook);
+      $(document).on('pagebeforeshow','#exercisePage',  app.prepareChapter);
+
+
     }, // onDeviceReady
 
-    dbMessage: function(msg) {
-      document.getElementById("divMsg").innerHTML = msg;
-    }, // end dbMessage
+    prepareBook: function (event) {
+      var bookId = app.currentBook.book ;
+      
+      document.getElementById("bookTitle").innerHTML = books[bookId].title;
+      
+      var chapters = document.getElementById('chapters');
+      
+      while (chapters.firstChild) chapters.removeChild(chapters.firstChild);
+     
+      for (var x = 0; x < books[bookId].length; x++) {
+      
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.setAttribute('data-id', x);
+        a.className = 'navChapter'
+         
+        var title = (x < 9) ? '0' + (x + 1) : (x + 1);
+        title = title + '. ' + books[bookId][x].title;
+        
+        var txt = document.createTextNode(title);
+        
+        a.appendChild(txt);
+        li.appendChild(a);
+        chapters.appendChild(li);
+      }
 
+      $('#chapters').listview('refresh');
+      
+      $(".navChapter").on('click', function () {
+        app.currentBook.chapter = parseInt(this.getAttribute("data-id"));
+        app.currentBook.sentence = 0;
+        $.mobile.changePage('#exercisePage');
+        return false;
+      });
+    },
+
+    prepareChapter: function(event) {
+      alert(app.currentBook.book + ' ' + app.currentBook.chapter + ' ' + app.currentBook.sentence);
+    },
+    
+    currentBook: {
+      book: -1,
+      chapter: -1,
+      sentence: -1
+    },
+    
     receivedEvent: function(id) {
     } // receivedEvent
 };
