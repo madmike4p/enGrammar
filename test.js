717ab0f894da35294da660bcd35cfe09394ef097
mike <mike@localhost>
2019-10-21 14:57:15 +0200

    przegladanie kart na test.html

---
+++ test.js
@@ -0,0 +1,15 @@
+var style = {
+  'font-size': '20px',
+  'width': '300px',
+  'padding': '30px'
+};
+
+for (prop in style) {
+  console.log(prop + " " + style[prop]);
+}
+
+var jsonStr = JSON.stringify(style);
+console.log(jsonStr);
+
+var newObj = JSON.parse(jsonStr);
+console.log(newObj['font-size']);
--- www/js/page_test.js
+++ www/js/page_test.js
@@ -1,3 +1,7 @@
+var book = 0;
+var chapter = 0;
+var exercise = 0;
+
 var app = {
 
     initialize: function() {
@@ -6,8 +10,59 @@ var app = {
 
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('button').addEventListener('click', this.btnClick, false);
+        document.getElementById('nextChapter').addEventListener('click', this.nextChapter, false);
+        document.getElementById('prevChapter').addEventListener('click', this.prevChapter, false);
     }, // end bindEvents
-    
+
     onDeviceReady: function() {
-    }
+      app.showCard('show');
+    }, 
+
+    prevChapter: function() {
+      if(--chapter < 0) chapter = books[book].length - 1;
+      exercise = 0;
+      document.getElementById('button').setAttribute('data-click', '0');
+      app.showCard('show');
+    },
+
+    nextChapter: function() {
+      if(++chapter >= books[book].length) chapter = 0;
+      exercise = 0;
+      document.getElementById('button').setAttribute('data-click', '0');
+      app.showCard('show');
+    },
+
+    showCard: function(state) {
+      var pl = document.getElementById('pl');
+      var en = document.getElementById('en');
+      var btn = document.getElementById('button');
+
+      switch (state) {
+        case 'showEn':
+          en.style.display = 'block';
+          break;
+        case 'next':
+          exercise++;
+          if (exercise >= books[book][chapter].length) exercise = 0;
+        case 'show':
+          en.style.display = 'none';
+          button.setAttribute('data-click', '0');
+          pl.innerHTML = books[book][chapter][exercise].pl;
+          en.innerHTML = books[book][chapter][exercise].en;
+          break;
+      }
+      document.getElementById('divMsg').innerHTML = (exercise + 1) + '/' + books[book][chapter].length;
+      document.getElementById('chapterTitle').innerHTML = books[book][chapter].title;
+    },
+
+    btnClick: function(event) {
+      if (this.getAttribute('data-click') == '0') {
+        app.showCard('showEn');
+        this.setAttribute('data-click', '1');
+      } else {
+        app.showCard('next');
+        this.setAttribute('data-click', '0');
+      }
+    } 
 };
