7afd6c26e817e497f751f33aae6e76ea1ca52929
mike <mike@localhost>
2019-10-24 00:54:41 +0200

    changes

--- _data/http/make.js
+++ _data/http/make.js
@@ -2,30 +2,63 @@ const fs = require('fs');
 
 var header = fs.readFileSync('header.html', 'utf-8');
 var footer = fs.readFileSync('footer.html', 'utf-8');
-
-var content = fs.readFileSync('info.txt', 'utf-8');
+var content = fs.readFileSync('Info.txt', 'utf-8');
 
 var chapters = content.split('-----');
 
-var html = [];
+function makeTable(tab) {
+  var chapter = tab.split('\n');
+  var start = -1;
+  var stop = -1;
 
-var current = chapters[0];
+  for (var x = 0; x < chapter.length; x++) {
 
-//pobieram numer
-chapter = current.split('\n');
-var nr = chapter.shift();
+    if (chapter[x].indexOf('|') > -1) {
+      if(start == -1) start = x;
+      stop = x;
+      var parts = chapter[x].split('|');
+      for (var z = 0; z < parts.length; z++) {
+        parts[z] = parts[z].trim();
+      }
+      parts = '<tr><td>' + parts.join('</td><td>') + '</td></tr>';
+      chapter[x] = parts;
+      } else {
+      chapter[x] = '<p>' + chapter[x] + '</p>';
+      }
+  }
 
-chapter = chapter.join('\n').trim().split('\n');
+  if (start != -1) {
+    chapter[start] = '<table>' + chapter[start];
+    chapter[stop] = chapter[stop] + '</table>';
+  }
 
-for (var x = 0; x < chapter.length; x++) {
-  if (chapter[x].trim() == '') chapter[x] = '***';
+  return chapter.join('\n');
+
 }
 
-chapter = chapter.join('\n').split('***');
+for (var x = 0; x < chapters.length; x++) {
+  if (x != 2) continue;
 
+  var chapter = chapters[x];
 
-console.log(chapter);
+  //pobieram numer
+  chapter = chapter.split('\n');
+  var nr = chapter.shift();
 
+  chapter = chapter.join('\n').trim().split('\n');
+  for (var y = 0; y < chapter.length; y++) {
+    if (chapter[y].trim() == '') chapter[y] = '***';
+    }
+  chapter = chapter.join('\n').split('***\n');
+  for (var y = 0; y < chapter.length; y++) {
+    chapter[y] = chapter[y].trim();
+    chapter[y] = makeTable(chapter[y]);
+    //makeTable(chapter[y]);
+  }
+
+  // tutaj mam wszysko podzielone na chmurki
+  console.log(chapter);
+}
 
 
 
@@ -34,4 +67,4 @@ console.log(chapter);
 
 header = header + '\n\n\n';
 footer = '\n\n\n' + footer;
-fs.writeFileSync('index.html', header + html.join('\n') + footer);
+// fs.writeFileSync('index.html', header + html.join('\n') + footer);
